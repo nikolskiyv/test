cb730c0ba7643b4e4baf6bd05f615ef50d71539f
nikolskiyv <50186930+nikolskiyv@users.noreply.github.com>
2021-12-23 03:50:51 +0300

    Add files via upload

---
+++ 409_nekrasov/PerceptionComponent/ImgDescription.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace PerceptionComponent
+{
+    public class ImgDescription
+    {
+        string imgName;
+        ConcurrentBag<ObjDescription> objs;
+        public ImgDescription()
+        {
+
+        }
+        public ImgDescription(string imgName)
+        {
+            this.imgName = imgName;
+            objs = new ConcurrentBag<ObjDescription>();
+        }
+        public void Add(ObjDescription obj)
+        {
+            objs.Add(obj);
+        }
+        public override string ToString()
+        {
+            string res = imgName + "\n";
+            foreach(ObjDescription desc in objs)
+            {
+                res += desc.ToString();
+            }
+            return res;
+        }
+        public string ImgName
+        {
+            get
+            {
+                return imgName;
+            }
+        }
+        public ConcurrentBag<ObjDescription> Objs
+        {
+            get
+            {
+                return objs;
+            }
+            set
+            {
+                objs = value;
+            }
+        }
+    }
+}
